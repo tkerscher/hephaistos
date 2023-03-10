4f84fb2ab4524d6cfd7665bdfd6fbd0f3541f72e
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-03-10 01:26:53 +0100

    updated deviceinfo example

--- examples/deviceinfo/deviceinfo.cpp
+++ examples/deviceinfo/deviceinfo.cpp
@@ -7,6 +7,19 @@ int main() {
     //print header
     std::cout << "Hephaistos v" << hp::VERSION_MAJOR << "." << hp::VERSION_MINOR << "." << hp::VERSION_PATCH << std::endl;
 
+    //fetch devices
+    auto devices = hp::enumerateDevices();
+    std::cout << devices.size() << " device(s) found.\n";
+
+    std::cout << std::boolalpha;
+    //iterate over all devices;
+    for (auto& device : devices) {
+        auto info = hp::getDeviceInfo(device);
+        std::cout << "\n";
+        std::cout << "Name:     " << info.name << '\n';
+        std::cout << "Discrete: " << info.isDiscrete << '\n';
+    }
+
     //done
     return 0;
 }
