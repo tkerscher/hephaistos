c6ae539bfe0eeb6e53da5a9a403b1853e62e3615
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-03-11 01:07:24 +0100

    added tensor to python api

--- python/buffer.cpp
+++ python/buffer.cpp
@@ -39,7 +39,7 @@ public:
     virtual ~TypedBuffer() = default;
 };
 template<class T>
-void registerTypedBuffer(nb::module_& m, const char* name) {
+void registerBuffer(nb::module_& m, const char* name) {
     nb::class_<TypedBuffer<T>>(m, name)
         .def(nb::init<size_t>())
         .def_prop_ro("size", [](const TypedBuffer<T>& b) { return b.size(); }, "The number of elements in this buffer.")
@@ -47,21 +47,49 @@ void registerTypedBuffer(nb::module_& m, const char* name) {
         .def("numpy", &TypedBuffer<T>::numpy, "Returns a numpy array using this buffer's memory.");
 }
 
+template<class T>
+class PyTensor : public hp::Tensor<T> {
+public:
+    PyTensor(size_t count) : hp::Tensor<T>(getCurrentContext(), count) {}
+    virtual ~PyTensor() = default;
+};
+template<class T>
+void registerTensor(nb::module_& m, const char* name) {
+    nb::class_<PyTensor<T>>(m, name)
+        .def(nb::init<size_t>())
+        .def_prop_ro("size", [](const PyTensor<T>& b) { return b.size(); }, "The number of elements in this tensor.")
+        .def_prop_ro("size_bytes", [](const PyTensor<T>& b) { return b.size_bytes(); }, "The size of the tensor in bytes.");
+}
+
 void registerBufferModule(nb::module_& m) {
     nb::class_<RawBuffer>(m, "RawBuffer")
         .def(nb::init<uint64_t>())
         .def_prop_ro("address", [](const RawBuffer& b) { return b.getAddress(); } , "The memory address of the allocated buffer.")
-        .def_prop_ro("size", [](const RawBuffer& b) { return b.size(); }, "The size of the buffer in bytes.");
+        .def_prop_ro("size", [](const RawBuffer& b) { return b.size(); }, "The size of the buffer in bytes.")
+        .def_prop_ro("size_bytes", [](const RawBuffer& b) { return b.size_bytes(); }, "The size of the buffer in bytes.");
 
-    //Register typed arrays
-    registerTypedBuffer<float>(m, "FloatBuffer");
-    registerTypedBuffer<double>(m, "DoubleBuffer");
-    registerTypedBuffer<uint8_t>(m, "ByteBuffer");
-    registerTypedBuffer<uint16_t>(m, "UnsignedShortBuffer");
-    registerTypedBuffer<uint32_t>(m, "UnsignedIntBuffer");
-    registerTypedBuffer<uint64_t>(m, "UnsignedLongBuffer");
-    registerTypedBuffer<int8_t>(m, "CharBuffer");
-    registerTypedBuffer<int16_t>(m, "ShortBuffer");
-    registerTypedBuffer<int32_t>(m, "IntBuffer");
-    registerTypedBuffer<int64_t>(m, "LongBuffer");
+    //Register typed buffers
+    registerBuffer<float>(m, "FloatBuffer");
+    registerBuffer<double>(m, "DoubleBuffer");
+    registerBuffer<uint8_t>(m, "ByteBuffer");
+    registerBuffer<uint16_t>(m, "UnsignedShortBuffer");
+    registerBuffer<uint32_t>(m, "UnsignedIntBuffer");
+    registerBuffer<uint64_t>(m, "UnsignedLongBuffer");
+    registerBuffer<int8_t>(m, "CharBuffer");
+    registerBuffer<int16_t>(m, "ShortBuffer");
+    registerBuffer<int32_t>(m, "IntBuffer");
+    registerBuffer<int64_t>(m, "LongBuffer");
+
+    //Register typed buffers
+    registerTensor<float>(m, "FloatTensor");
+    registerTensor<double>(m, "DoubleTensor");
+    registerTensor<uint8_t>(m, "ByteTensor");
+    registerTensor<uint16_t>(m, "UnsignedShortTensor");
+    registerTensor<uint32_t>(m, "UnsignedIntTensor");
+    registerTensor<uint64_t>(m, "UnsignedLongTensor");
+    registerTensor<int8_t>(m, "CharTensor");
+    registerTensor<int16_t>(m, "ShortTensor");
+    registerTensor<int32_t>(m, "IntTensor");
+    registerTensor<int64_t>(m, "LongTensor");
 }
+
--- python/hephaistos/util.py
+++ python/hephaistos/util.py
@@ -1,11 +1,10 @@
 import ctypes
 from typing import Any
-from .pyhephaistos import RawBuffer
-
+from .pyhephaistos import RawBuffer, ByteTensor
 
 class StructureBuffer(RawBuffer):
     """
-    Helper class for creating types buffers.
+    Helper class for creating typed buffers.
     """
 
     def __init__(self, type: ctypes.Structure):
@@ -17,9 +16,23 @@ class StructureBuffer(RawBuffer):
         super().__setattr__("_pointer", ctypes.cast(super().address, ctypes.POINTER(type)))
     
     def __getattribute__(self, __name: str):
+        #special types: size, size_bytes
+        if __name in ["size", "size_bytes"]:
+            return super().__getattribute__(__name)
         # bypass __getattribute__ to prevent infinite loop
         return super().__getattribute__("_pointer").contents.__getattribute__(__name)
     
     def __setattr__(self, __name: str, __value: Any) -> None:
         # bypass our own __getattribute__
         super().__getattribute__("_pointer").contents.__setattr__(__name, __value)
+
+class StructureTensor(ByteTensor):
+    """
+    Helper class for creating tensors matching the size of a given struct.
+    """
+
+    def __init__(self, type: ctypes.Structure):
+        """
+        Creates a tensor with the exact size to hold the given structure.
+        """
+        super().__init__(ctypes.sizeof(type))
