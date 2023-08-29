3875e346ca0460a1807ddc1feb70a831d87842e6
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-08-29 17:32:50 +0200

    bumped version to 0.2.0

--- src/version.cpp
+++ src/version.cpp
@@ -1,5 +1,5 @@
 #include "hephaistos/version.hpp"
 
 const uint32_t hephaistos::VERSION_MAJOR = 0;
-const uint32_t hephaistos::VERSION_MINOR = 1;
+const uint32_t hephaistos::VERSION_MINOR = 2;
 const uint32_t hephaistos::VERSION_PATCH = 0;
