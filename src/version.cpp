1a888eb3c29a643e86c248c8dd3e3b01a190d90b
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-11-17 15:06:21 +0100

    pushed version in cpp part

--- src/version.cpp
+++ src/version.cpp
@@ -1,5 +1,5 @@
 #include "hephaistos/version.hpp"
 
 const uint32_t hephaistos::VERSION_MAJOR = 0;
-const uint32_t hephaistos::VERSION_MINOR = 4;
+const uint32_t hephaistos::VERSION_MINOR = 5;
 const uint32_t hephaistos::VERSION_PATCH = 0;
