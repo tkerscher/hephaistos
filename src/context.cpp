9bd63c2009f816aa2f039d2471803b37aface6e5
tkerscher <88444139+tkerscher@users.noreply.github.com>
2025-12-25 02:51:08 +0100

    added appropriate flags to vma allocator when creating

--- src/context.cpp
+++ src/context.cpp
@@ -199,6 +199,15 @@ DeviceInfo getDeviceInfo(const ContextHandle& context) {
 
 namespace {
 
+constexpr auto VmaAllocatorFlags =
+    VMA_ALLOCATOR_CREATE_KHR_DEDICATED_ALLOCATION_BIT |
+    VMA_ALLOCATOR_CREATE_KHR_BIND_MEMORY2_BIT |
+    VMA_ALLOCATOR_CREATE_KHR_MAINTENANCE4_BIT |
+    VMA_ALLOCATOR_CREATE_KHR_MAINTENANCE5_BIT |
+    // VMA_ALLOCATOR_CREATE_EXT_MEMORY_BUDGET_BIT |
+    // VMA_ALLOCATOR_CREATE_EXT_MEMORY_PRIORITY_BIT |
+    VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT;
+
 void destroyContext(vulkan::Context* context) {
     if (context->allocator)
         vmaDestroyAllocator(context->allocator);
@@ -445,7 +454,7 @@ ContextHandle createContext(
         };
 
         VmaAllocatorCreateInfo info{
-            .flags            = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
+            .flags            = VmaAllocatorFlags,
             .physicalDevice   = device,
             .device           = context->device,
             .pVulkanFunctions = &functions,
